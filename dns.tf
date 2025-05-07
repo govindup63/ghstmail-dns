5cb48316355fa806d216bed3bed4ee791fccf72d
Govind <govindup63@gmail.com>
2025-05-07 22:07:52 +0530

    update DNS record configuration for pb server with new IP address

--- dns.tf
+++ dns.tf
@@ -9,11 +9,11 @@
 #   proxied = false
 # }
 
-resource "cloudflare_dns_record" "a_root" {
+resource "cloudflare_dns_record" "pb_server" {
   zone_id = var.zone_id
-  name    = "test"
-  type    = "A"
-  content = "23.45.12.23"
+  name = "pb"
+  type = "A"
+  content = "69.62.77.147"
   ttl     = 1
   proxied = false
 }
