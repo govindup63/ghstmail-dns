b424ca60ef60f948a049ea075cfe51f6be3ceff5
Govind <govindup63@gmail.com>
2025-05-07 19:27:21 +0530

    testing

---
+++ dns.tf
@@ -0,0 +1,10 @@
+# template for dns records
+
+# resource "cloudflare_dns_record" "a_root" {
+#   zone_id = var.zone_id
+#   name    = "@"
+#   type    = "A"
+#   content = "23.45.12.23"
+#   ttl     = 1
+#   proxied = false
+# }
