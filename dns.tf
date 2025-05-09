35227e5dba820c1cc83ada5344fd28ebc66a1aae
Govind <66307025+govindup63@users.noreply.github.com>
2025-05-09 18:43:10 +0530

    Revert "Added dns records"

--- dns.tf
+++ dns.tf
@@ -9,50 +9,10 @@
 #   proxied = false
 # }
 
-# resource "cloudflare_dns_record" "pb_server" {
-#  zone_id = var.zone_id
-#  name = "pb"
-#  type = "A"
-#  content = "69.62.77.147"
-#  ttl     = 1
-#  proxied = false
-# }
-
-# blog.pointblank.club → Ghost blog
-resource "cloudflare_dns_record" "blog_pointblank" {
-  zone_id = var.zone_id_pointblank
-  name    = "blog"
-  type    = "A"
-  content = "69.62.77.147"
-  ttl     = 1
-  proxied = false
-}
-
-# ghost-admin.dsce.in → Ghost admin panel
-resource "cloudflare_dns_record" "ghost_admin" {
-  zone_id = var.zone_id_dsce
-  name    = "ghost-admin"
-  type    = "A"
-  content = "69.62.77.147"
-  ttl     = 1
-  proxied = false
-}
-
-# writeups-origin.dsce.in → Ghost mirror
-resource "cloudflare_dns_record" "writeups_origin" {
-  zone_id = var.zone_id_dsce
-  name    = "writeups-origin"
-  type    = "A"
-  content = "69.62.77.147"
-  ttl     = 1
-  proxied = false
-}
-
-# forum.dsce.in → Forum 
-resource "cloudflare_dns_record" "forum" {
-  zone_id = var.zone_id_dsce
-  name    = "forum"
-  type    = "A"
+resource "cloudflare_dns_record" "pb_server" {
+  zone_id = var.zone_id
+  name = "pb"
+  type = "A"
   content = "69.62.77.147"
   ttl     = 1
   proxied = false
