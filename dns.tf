4cfb65b67bdc25864ad45a3b6a8124fa1c06bc3f
Sky Singh <114267538+SkySingh04@users.noreply.github.com>
2025-05-09 23:53:39 +0530

    maa chod di DNS ki?

--- dns.tf
+++ dns.tf
@@ -9,14 +9,14 @@
 #   proxied = false
 # }
 
-#resource "cloudflare_dns_record" "pb_server" {
-#  zone_id = var.zone_id
-#  name = "pb"
-#  type = "A"
-#  content = "69.62.77.147"
-#  ttl     = 1
-#  proxied = false
-#}
+resource "cloudflare_dns_record" "pb_server" {
+  zone_id = var.zone_id
+  name = "pb"
+  type = "A"
+  content = "69.62.77.147"
+  ttl     = 1
+  proxied = false
+}
 
 # blog.pointblank.club → Ghost blog
 resource "cloudflare_dns_record" "blog_pointblank" {
